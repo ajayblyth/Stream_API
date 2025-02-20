2aa1ed63ca9662172c3a808890c1e18ec88a5e74
ajayblyth <ajayblyth@gmail.com>
2025-02-20 20:13:57 +0530

    FP03/MethodReference

---
+++ src/udemy_Stream_Api/FP03/MethodRefernce.java
@@ -0,0 +1,19 @@
+package udemy_Stream_Api.FP03;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MethodRefernce {
+    public static void main(String[] args) {
+        List<String> names = List.of("ajay", "sharma", " abc"," defg",  "kunla", "jammu");
+//        names.stream()
+//        .map(x-> x.toUpperCase())
+//        .forEach(System.out::println);
+
+        List<String> result =names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(result);
+
+    }
+}
