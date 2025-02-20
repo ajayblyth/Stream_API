e234dd4d5b0919d93df9e11522838b354883fe71
ajayblyth <ajayblyth@gmail.com>
2025-02-20 15:14:57 +0530

    FP02/reduceByLambda/reduceByBuiltInMethod

--- src/udemy_Stream_Api/FP_02/Reduce.java
+++ src/udemy_Stream_Api/FP_02/Reduce.java
@@ -6,10 +6,23 @@ import java.util.List;
 public class Reduce {
     public static void main(String[] args) {
         List<Integer> numbers =Arrays.asList(23,234,56,77,89,76,543,32);
-        int  result = reduceMethod(numbers);
-        System.out.println(result);
+//        int  result = reduceMethod(numbers);
+//        System.out.println(result);
+        reduceByLambda(numbers);
+        reduceByBuiltInMethods(numbers);
     }
-private static int sum(int a, int b){
+
+    private static void reduceByBuiltInMethods(List<Integer> numbers) {
+        int addition = numbers.stream().reduce(0, Integer::sum);
+        System.out.println("addition = " + addition);
+    }
+
+    private static void reduceByLambda(List<Integer> numbers) {
+        int sum = numbers.stream().reduce(0,(x,y)-> x+y);
+        System.out.println("sum = " + sum);
+    }
+
+    private static int sum(int a, int b){
         return a+b;
 }
     private static int reduceMethod(List<Integer> numbers) {
