285680917b0a88213c0119132c7e14924fae03ea
ajayblyth <ajayblyth@gmail.com>
2025-02-20 13:40:42 +0530

    methodReference

---
+++ src/udemy_Stream_Api/MethodReference.java
@@ -0,0 +1,17 @@
+package udemy_Stream_Api;
+
+import java.util.List;
+
+public class MethodReference {
+    public static void main(String[] args) {
+        printAllNumbers(List.of(1,3,4,5,7,3,4));
+
+    }
+    private static void print(int number){
+        System.out.println(number);
+    }
+
+    private static void printAllNumbers(List<Integer> numbers) {
+       numbers.stream().forEach(MethodReference::print); //method refernce
+        }
+    }
