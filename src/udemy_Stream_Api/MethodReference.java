6735692ab3d75504c7593ddc47cf95c03938f101
ajayblyth <ajayblyth@gmail.com>
2025-02-20 14:02:40 +0530

    isEvenUsingLambda

--- src/udemy_Stream_Api/MethodReference.java
+++ src/udemy_Stream_Api/MethodReference.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class MethodReference {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 3, 4, 5, 7, 3, 8,10);
+        List<Integer> numbers = List.of(1, 3, 4,9 ,6,5, 7, 3, 8,10);
 //        printAllNumbers(numbers);
-        evenNumber(numbers);
+//        evenNumber(numbers);
+        isEvenUsinglambda(numbers);
 
     }
     private static boolean isEven(int number) {
@@ -26,5 +27,11 @@ public class MethodReference {
 
     private static void printAllNumbers(List<Integer> numbers) {
        numbers.stream().forEach(MethodReference::print); //method refernce
+        }
+
+
+        private static void isEvenUsinglambda(List<Integer> numbers){
+        numbers.stream().filter(x-> x%2 == 0).forEach(System.out::println);
+
         }
     }
