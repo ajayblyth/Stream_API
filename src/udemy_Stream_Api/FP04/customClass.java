0d8caa0e2a1113d7fed524f446e0f10617640e17
ajayblyth <ajayblyth@gmail.com>
2025-02-20 21:13:17 +0530

    FP03/customClass

--- src/udemy_Stream_Api/FP04/customClass.java
+++ src/udemy_Stream_Api/FP04/customClass.java
@@ -3,55 +3,6 @@ package udemy_Stream_Api.FP04;
 
 import java.util.List;
 
-class Course{
-    public Course(String name, String category,int noOfStudents, int reviewScore) {
-        this.name = name;
-        this.noOfStudents = noOfStudents;
-        this.reviewScore = reviewScore;
-        this.category = category;
-    }
-
-    private String name;
-    private String category;
-    private int reviewScore;
-    private int noOfStudents;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getReviewScore() {
-        return reviewScore;
-    }
-
-    public void setReviewScore(int reviewScore) {
-        this.reviewScore = reviewScore;
-    }
-
-    public int getNoOfStudents() {
-        return noOfStudents;
-    }
-
-    public void setNoOfStudents(int noOfStudents) {
-        this.noOfStudents = noOfStudents;
-    }
-
-    public String toString(){
-        return name + ":" + noOfStudents + ": " + reviewScore;
-    }
-}
 public class customClass {
 
 
@@ -74,7 +25,7 @@ public class customClass {
                 new Course("Docker","Framework", 98 , 20000),
                 new Course("Kubernates","Framework", 98 , 20000),
 
-                )
+                );
 
     }
 }
