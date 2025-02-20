55473b9b7a8cc0d15f93a756de22ca1c7a521f65
ajayblyth <ajayblyth@gmail.com>
2025-02-20 15:27:46 +0530

    FP02/max,min using reduce

--- src/udemy_Stream_Api/FP_02/Reduce.java
+++ src/udemy_Stream_Api/FP_02/Reduce.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public class Reduce {
     public static void main(String[] args) {
-        List<Integer> numbers =Arrays.asList(23,234,56,77,89,76,543,32);
+        List<Integer> numbers =Arrays.asList(23,234,56,77,-89,76,543,32);
 //        int  result = reduceMethod(numbers);
 //        System.out.println(result);
         reduceByLambda(numbers);
         reduceByBuiltInMethods(numbers);
+        maxNumberUsingReduce(numbers);
+        minNumberUsingReduce(numbers);
+    }
+
+    private static void minNumberUsingReduce(List<Integer> numbers) {
+        int min = numbers.stream().reduce(Integer.MAX_VALUE, (x, y) -> x > y ? y : x);
+        System.out.println("min = " +min);
+    }
+    //Note use Interger.Min_value to deal with negative numbers as well
+
+    private static void maxNumberUsingReduce(List<Integer> numbers) {
+        int max = numbers.stream().reduce(Integer.MIN_VALUE, (x, y) -> x > y ? x : y);
+        System.out.println("max= " + max);
     }
 
     private static void reduceByBuiltInMethods(List<Integer> numbers) {
