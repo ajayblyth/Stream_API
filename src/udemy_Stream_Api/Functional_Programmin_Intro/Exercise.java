1ae25f8fc39e23a5c0d7c73dbe9f06e0c3504792
ajayblyth <ajayblyth@gmail.com>
2025-02-20 14:39:32 +0530

    Exercise/cubeOfOdd/charactersInEachcourseName

--- src/udemy_Stream_Api/Functional_Programmin_Intro/Exercise.java
+++ src/udemy_Stream_Api/Functional_Programmin_Intro/Exercise.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class Exercise {
     public static void main(String[] args) {
        List<Integer> list = Arrays.asList(12,45,61,87,54,67,89,32);
-       isOddNumber(list);
-       courses();
+//       isOddNumber(list);
+          courses();
+//       cubeOfOddNumbers(list);
+    }
+
+    private static void cubeOfOddNumbers(List<Integer> list) {
+        list.stream().filter(x-> x%2 !=0).map(x-> x*x*x).forEach(System.out::println);
     }
 
     private static void isOddNumber(List<Integer> list) {
@@ -16,8 +21,9 @@ public class Exercise {
     private static void courses(){
         List <String > course = Arrays.asList("spring","spring boot", "api", "kubernate", "micro service ", "abc");
 
-      course.stream().filter(x -> x.contains("spring")).forEach(System.out::println);
+//      course.stream().filter(x -> x.contains("spring")).forEach(System.out::println);
 
-        course.stream().filter(x -> x.length()>=4).forEach(System.out::println);
+//        course.stream().filter(x -> x.length()>=4).forEach(System.out::println);
+        course.stream().map(x ->x + "-> " + x.length() + "characters").forEach(System.out::println);
     }
 }
