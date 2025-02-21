9448330aead232014c94ebf44c86c423195064fa
ajayblyth <ajayblyth@gmail.com>
2025-02-21 09:57:40 +0530

    FP03/Comparator.comparing

--- src/udemy_Stream_Api/FP04/customClass.java
+++ src/udemy_Stream_Api/FP04/customClass.java
@@ -1,8 +1,10 @@
 package udemy_Stream_Api.FP04;
 
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 class Course {
@@ -60,40 +62,58 @@ class Course {
 public class customClass {
 
 
-
     public static void main(String[] args) {
-        List<Course> courses= List.of(
-                new Course("Spring","Framework", 98 , 20000),
-                new Course("Spring Boot","Framework", 95 , 18000),
+        List<Course> courses = List.of(
+                new Course("Spring", "Framework", 98, 20000),
+                new Course("Spring Boot", "Framework", 95, 18000),
 
-                new Course("API","Microservices", 97 , 22000),
+                new Course("API", "Microservices", 97, 22000),
 
-                new Course("MicroServices","Framework", 96 , 25000),
+                new Course("MicroServices", "Framework", 96, 25000),
 
-                new Course("FullStack","fullstack", 91 , 14000),
+                new Course("FullStack", "fullstack", 91, 14000),
 
-                new Course("Aws","cloud", 92 , 21000),
+                new Course("Aws", "cloud", 92, 21000),
 
-                new Course("Azure","cloud", 99 , 24000),
+                new Course("Azure", "cloud", 99, 24000),
 
-                new Course("Docker","cloud", 92 , 20000),
-                new Course("Kubernates","cloud", 91 , 20000)
+                new Course("Docker", "cloud", 92, 20000),
+                new Course("Kubernates", "cloud", 91, 20000)
 
-                );
-        System.out.println(courses.stream().allMatch(getReviewScoreGreateThan90()));
-        System.out.println(courses.stream().anyMatch(getReviewScoreGreateThan95()));
-        System.out.println(courses.stream().noneMatch(getReviewScoreLessThan90()));
+        );
+//Note: at the time of primitive data type , use primitives like comparingInt/comparingDouble as it takes autoboxing/unboxing out of picture
+// and improves performance
+
+        Comparator<Course> comparingWithNumberOfStudents = Comparator.comparingInt(Course::getNoOfStudents); // using comparingInt as NoOfStudents is of primitive type
+
+        Comparator<Course> comparingWithNumberOfStudentsIncreasing = Comparator.comparing(Course::getNoOfStudents).reversed();
+
+        Comparator<Course> comparingWithNumberOfstudentsAndNoOfReviews = Comparator
+                .comparing(Course::getNoOfStudents)
+                .thenComparing(Course::getReviewScore);
+
+//        System.out.println(courses.stream().allMatch(getReviewScoreGreateThan90()));
+//        System.out.println(courses.stream().anyMatch(getReviewScoreGreateThan95()));
+//        System.out.println(courses.stream().noneMatch(getReviewScoreLessThan90()));
+
+//        System.out.println(courses.stream().sorted(comparingWithNumberOfStudents).collect(Collectors.toList()));
+//
+//        System.out.println(courses.stream().sorted(comparingWithNumberOfStudentsIncreasing).collect(Collectors.toList()));
+
+        System.out.println(courses.stream().sorted(comparingWithNumberOfstudentsAndNoOfReviews).collect(Collectors.toList()));
 
 
     }
+
 
     private static Predicate<Course> getReviewScoreGreateThan90() {
         return course -> course.getReviewScore() > 90;
     }
 
     private static Predicate<Course> getReviewScoreGreateThan95() {
-        return course -> course.getReviewScore() >95;
+        return course -> course.getReviewScore() > 95;
     }
+
     private static Predicate<Course> getReviewScoreLessThan90() {
         return course -> course.getReviewScore() < 90;
     }
