8f49497236b40d7e307f7076aba06fac39289e7b
ajayblyth <ajayblyth@gmail.com>
2025-02-22 13:27:03 +0530

    psa/groupingBy/forEach

--- src/psa_stream/E.java
+++ src/psa_stream/E.java
@@ -2,13 +2,14 @@ package psa_stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class E {
     public static void main(String[] args) {
         List<Employee> data = Arrays.asList(
                 new Employee(1, "Mike", 20000),
-                new Employee(2, "Adam", 25000),
+                new Employee(2, "Adam", 20000),
                 new Employee(3, "Smith", 22000),
                 new Employee(4, "mike", 22000),
                 new Employee(5, "kyoto", 18000)
@@ -38,6 +39,16 @@ public class E {
                 .collect(Collectors.toList());
 
        salaryHike.forEach(System.out::println);
+
+
+      Map<Integer,List<Employee>> result= data.stream().collect(Collectors.groupingBy(Employee::getSalary));
+        System.out.println(result);
+
+        data.stream().forEach(e->e.getName());
+
+        List<Integer> numbers = Arrays.asList(12,23,34,456,67);
+        numbers.stream().forEach(System.out::println);
     }
+
 
 }
